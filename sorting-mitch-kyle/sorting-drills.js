d90b7faea4f89c191dc98b4778180bb57e72f756
MitchGianoni <mitchgianoni@yahoo.com>
2018-09-06 16:32:38 -0400

    first two sorts complete

---
+++ sorting-mitch-kyle/sorting-drills.js
@@ -0,0 +1,100 @@
+'use strict';
+
+let array  = [89,30,25,32,72,70,51,42,25,24,53,55,78,50,13,40,48,32,26,2,14,33,45,72,56,44,21,88,27,68,15,62,93,
+  98,73,28,16,46,87,28,65,38,67,16,85,63,23,69,64,91,9,70,81,27,97,82,6,88,3,7,46,13,11,64,76,31,
+  26,38,28,13,17,69,90,1,6,7,64,43,9,73,80,98,46,27,22,87,49,83,6,39,42,51,54,84,34,53,78,40,14,5];
+
+function swap(array, i, j) {
+  const tmp = array[i];
+  array[i] = array[j];
+  array[j] = tmp;
+}
+
+function partition(array, start, end) {
+  const pivot = array[end-1];
+  let j = start;
+  for (let i=start; i<end - 1; i++) {
+    if (array[i] <= pivot) {
+      swap(array, i, j);
+      j++;
+    }
+  }
+  swap(array, end-1, j);
+  return j;
+}
+
+
+// QuickSort - Write a function qSort that sorts a dataset using the quicksort algorithm
+// Add functionality to the program so that it shows how many operations it took to sort the same dataset
+// O(nlog(n)) best/average, O(n^2) worst case - cache-efficient and can easily be performed in-place.
+let qCount = 0;
+function qSort(array, start = 0, end = array.length) {
+  if (start >= end) {
+    return array;
+  }
+  qCount++;
+  const middle = partition(array, start, end);
+  array = qSort(array, start, middle);
+  array = qSort(array, middle+1, end);
+  return array;
+}
+
+// console.time();
+// console.log(qSort(array), qCount, 'actions');
+// console.timeEnd();
+
+// MergeSort - Write a function mSort that sorts the dataset above using the mergesort algorithm.
+// Add functionality to the program so that it shows how many operations it took to sort the same dataset
+let mCount = 0;
+function mSort(array) {
+  if (array.length <= 1) {
+    return array;
+  }
+
+  mCount++;
+  const middle = Math.floor(array.length / 2);
+  let left = array.slice(0, middle);
+  let right = array.slice(middle, array.length);
+
+  left = mSort(left);
+  right = mSort(right);
+  return merge(left, right, array);
+}
+
+function merge(left, right, array) {
+  let leftIndex = 0;
+  let rightIndex = 0;
+  let outputIndex = 0;
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      array[outputIndex++] = left[leftIndex++];
+    }
+    else {
+      array[outputIndex++] = right[rightIndex++];
+    }
+  }
+
+  for (let i = leftIndex; i < left.length; i++) {
+    array[outputIndex++] = left[i];
+  }
+
+  for (let i = rightIndex; i < right.length; i++ ) {
+    array[outputIndex++] = right[i];
+  }
+  return array;
+}
+
+// console.time();
+// console.log(mSort(array), mCount, 'actions');
+// console.timeEnd();
+
+// BucketSort - Write an O(n) algorithm to sort an array of integers, where you know in advance what the lowest
+// and highest values are
+function bucketSort(integers) {
+
+}
+
+// Sort in place - Write an algorithm to shuffle an array into a random order in-place (without creating a new array)
+function sortInPlace(array) {
+
+}
